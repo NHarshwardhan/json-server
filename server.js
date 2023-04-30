968fa60df8e6c9061af35a1a45d3dc5faff3e952
nishant <nishantharshwardhan@gmail.com>
2023-05-01 01:29:57 +0530

    first commit6

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const jsonServer = require('json-server')
 const clone = require('clone')
 const data = require('./db.json')
+const cors = require('cors')
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
 const fs = require('fs')
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
-const router = jsonServer.router(db)
+
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-
+server.use(cors())
 server.use((req, res, next) => {
     if (req.path !== '/')
         router.db.setState(clone(data))
     next()
 })
 
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
