119c32671b8b1c6bf7b3ee57ed63872882f862da
nishant <nishantharshwardhan@gmail.com>
2023-05-01 01:11:10 +0530

    first commit3

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router('db.json', {
-    _isFake: isProductionEnv
-})
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
